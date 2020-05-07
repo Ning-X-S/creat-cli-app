8d74540cc0bf11b9dd8ec0bad86262220e5f654a
higo <higo@higodeMacBook-Pro.local>
2020-05-07 15:02:18 +0800

    ADD:按需载入

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,17 @@
-const { override, fixBabelImports } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
 module.exports = override(
-    fixBabelImports('import', {
-        libraryName: 'antd-mobile',
-        style: 'css',
-    }),
+  fixBabelImports('antd-mobile', {
+    style: 'css',
+  }),
+  fixBabelImports('antd', {
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    lessOptions: { // If you are using less-loader@5 please spread the lessOptions to options directly
+      javascriptEnabled: true,
+      modifyVars: { '@primary-color': '#1DA57A' },
+      // localIdentName: '[local]--[hash:base64:5]' // 自定义 CSS Modules 的 localIdentName
+    },
+  })
 );
