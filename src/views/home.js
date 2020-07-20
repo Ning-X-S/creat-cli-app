891af602919ba68e938d9721b08ed6ed1c75b7ce
higo <higo@higodeMacBook-Pro.local>
2020-07-20 13:38:31 +0800

    ADD:备案号

--- src/views/home.js
+++ src/views/home.js
@@ -179,6 +179,9 @@ class Home extends React.Component {
             name="functional"
           />
         </header>
+        <footor style={{marginTop: 100, textAlign: 'center', marginBottom: 40}}>
+            <a href="www.beian.miit.gov.cn">陕ICP备20005100号</a>
+        </footor>
       </div>
     )
   }
